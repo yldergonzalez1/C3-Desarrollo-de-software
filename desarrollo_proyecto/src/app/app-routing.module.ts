988451378435a908cf18a1dcf4d2afc1215e55f4
yldergonzalez1 <90263798+yldergonzalez1@users.noreply.github.com>
2021-10-18 15:30:45 -0500

    Avance Facturas Ricardo
    LLevamos:
    1. Landing page sencilla
    2. Header con menu de navegación
    3. Inicio de sesión sin validador
    4. Se incluye el formulario de registro del docente
    5. Se añade el trabajo de Ricardo con la plantilla y el css de facturación

--- desarrollo_proyecto/src/app/app-routing.module.ts
+++ desarrollo_proyecto/src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GestionDeUsuariosInicioDeSesionComponent } from "./funcion_Ylder/gestion-de-usuarios-inicio-de-sesion/gestion-de-usuarios-inicio-de-sesion.component";
-import { LandingPageComponent } from "./funcion_landing/landing-page/landing-page.component";
+import { NgModule, Component } from '@angular/core';
+import { GestionDeUsuariosInicioDeSesionComponent } from './Ylder/gestion-de-usuarios-inicio-de-sesion/gestion-de-usuarios-inicio-de-sesion.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { RegistrarUsuarioComponent } from './registrar-usuario/registrar-usuario.component';
+import { GestionDeFacturacionComponent } from './Ricardo/gestion-de-facturacion/gestion-de-facturacion.component';
 
-const routes: Routes = [];
+const routes: Routes =[
+{path: 'inicio', component: InicioComponent},
+{path: 'facturas', component: GestionDeFacturacionComponent},
+{path: 'login', component: GestionDeUsuariosInicioDeSesionComponent},
+{path: 'registrar usuario', component: RegistrarUsuarioComponent},
+{path: '', component: InicioComponent}
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
